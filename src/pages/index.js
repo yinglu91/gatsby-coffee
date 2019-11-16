92a8c4b1f5bc34776ec37c6b0152639c83222f77
Ying Lu <yinglu91@gmail.com>
2019-11-15 21:32:34 -0500

    added info and title to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaGulp } from "react-icons/fa"
-
-const IndexPage = () => (
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+    />
+    <Info />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
